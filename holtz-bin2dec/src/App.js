c8b6b0e616f8fc9cd97ae7607f84a22e119a91f1
HENRIQUE <henrique_holtz@hotmail.com>
2021-02-09 23:35:59 -0300

    Added some global styles with sass

--- holtz-bin2dec/src/App.js
+++ holtz-bin2dec/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import Card from './Components/Card'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2 style={{ display: 'inline-block'}} >Development from Henrique Holtz</h2>
-        <span className="float-right">@2021</span>
+        <h2 className="d-inline-block" >Development from Henrique Holtz</h2>
+        <span style={{ position: 'relative' }} className="float-right pr-10 text-10 t-50">@2021</span>
       </header>
       <body>
-
+        <Card />
       </body>
     </div>
   );
