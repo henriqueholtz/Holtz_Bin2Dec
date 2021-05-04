2413ec5a804780e7767838b5e7e63d4bb3c764a3
henriqueholtz <henrique_holtz@hotmail.com>
2021-05-03 23:29:00 -0300

    Adjusting css of inputs and button

--- holtz-bin2dec/src/Components/CustomInput/index.jsx
+++ holtz-bin2dec/src/Components/CustomInput/index.jsx
@@ -1,12 +1,12 @@
-import { TextField } from '@material-ui/core';
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import PropTypes from 'prop-types';
 
 import styles from '../CustomInput/styles.module.scss';
 
 const inputProps = {
-    min: 0,
-    color:"#83DBD0"
+    min: 0
 }
 
 export default function CustomInput({origin, value, type, onChange}) {
@@ -14,9 +14,25 @@ export default function CustomInput({origin, value, type, onChange}) {
         <div>
             <label className={styles.label} htmlFor={type}>{origin}</label>
             {/* <input className={styles.input} name={type} type="number" value={value} onChange={onChange} min={0} width={186} placeholder={type} /> */}
-            <TextField name={type} type="number" variant="outlined" value={value} onChange={onChange} inputProps={inputProps} InputProps={{
-            startAdornment: <InputAdornment position="start" style={{color: '#fff !important'}}>{type}</InputAdornment>,
-          }} width={186} color="#83DBD0 !important"/>
+            {/* <span id="textField">
+                <TextField id="x" size="medium" color="#FFF" name={type} type="number" variant="outlined" value={value} onChange={onChange} inputProps={inputProps} InputProps={{
+                startAdornment: <InputAdornment position="start" className={styles.inputAdornment} >{type}</InputAdornment>,
+                          }} color="#FFF !important"/>
+            </span> */}
+            <TextField
+                label="labelFix"
+                color="white"
+                id={type}
+                className={styles.input}
+                value={value}
+                onChange={onChange} 
+                type="number"
+                inputProps={inputProps}
+                InputProps={{
+                    startAdornment: <InputAdornment position="start" className={styles.inputAdornment} >{type}</InputAdornment>
+                }}
+                variant="outlined"
+            />
         </div>
     )
 }
--- holtz-bin2dec/src/Components/Footer/index.jsx
+++ holtz-bin2dec/src/Components/Footer/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import GithubIcon from './GithubIcon';
 import FacebookIcon from './FacebookIcon';
+import InstagramIcon from './InstagramIcon';
 
 import styles from '../Footer/styles.module.scss'
-import InstagramIcon from './InstagramIcon';
 
 export default function Footer() {
     return(
         <div className={styles.footer}>
             <a href="https://www.facebook.com/henrique.holtz.7" target="_blank" rel="noreferrer">
-                <FacebookIcon width={32} height={32} className={styles.facebookIcon} />
+                <FacebookIcon width={32} height={32} />
             </a>
             <a href="https://www.instagram.com/henrique_holtz/" target="_blank" rel="noreferrer">
                 <InstagramIcon width={32} height={32} className={styles.instagramIcon} />
