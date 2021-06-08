b29f7809f968b83edaee474848a2708f0875bc1d
henriqueholtz <henrique_holtz@hotmail.com>
2021-06-08 03:16:49 -0300

    Adjustments in layout (using flex-box)

--- holtz-bin2dec/src/App.jsx
+++ holtz-bin2dec/src/App.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core";
 import Footer from './Components/Footer';
 import Bin2Dec from './Components/Bin2Dec';
 
-import styles from '../src/styles/globalStyles.module.scss'
+import '../src/styles/globalStyles.scss'
 
 export default function App() {
   const classes = useStyles();
