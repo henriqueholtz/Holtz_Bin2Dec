b5707e5c9ce5d22d20ad74c74a2c3e94e9d044f3
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-31 09:59:01 -0300

    Fix: "Error: No duplicate props allowed"

--- Components/Bin2Dec/ChangeThemeIcon.jsx
+++ Components/Bin2Dec/ChangeThemeIcon.jsx
@@ -2,7 +2,6 @@ export function ChangeThemeIcon(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      xmlns="http://www.w3.org/1999/xlink"
       version="1.1"
       id="mdi-theme-light-dark"
       width="24"
